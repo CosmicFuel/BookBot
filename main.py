4e162e0d66fae95061b6a270e5f1e2e6dab30f78
CosmicFuel <ethanrhits@gmail.com>
2025-07-02 13:46:34 -0500

    counts words now

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("greetings boots")
+def get_book_text(filepath):
+    try:
+        with open(filepath,"r") as f:
+            content = f.read()
+        return content
+    except FileNotFoundError:
+        return "Error: File not found."
+
+def main():
+    content = get_book_text("books/frankenstein.txt")
+    word_count(content)
+
+def word_count(content):
+    all_words = content .split()
+    num_words = len(all_words)
+    print(num_words, "words found in the document")
+
+main()
+
+
